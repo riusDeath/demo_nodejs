243dd52ceac94aaa5b60f415e40b9687c604b08e
riusdeath <vuthithuy10197@gmail.com>
2021-04-11 00:13:30 +0700

    check code

--- app/controllers/LogController.js
+++ app/controllers/LogController.js
@@ -4,6 +4,15 @@ var mongoose = require('mongoose');
 const log = require('../models/log');
 
 const fs = require('fs');
+const uri = "mongodb://localhost:27017/system_report"
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=> {
+    console.log('Database connected');
+  })
+  .catch((error)=> {
+    console.log('Error connecting to database');
+  });
+
 
 fs.readFile(path_config, (err, data) => {
     if (err) throw err;
@@ -22,11 +31,13 @@ fs.readFile(path_config, (err, data) => {
     features.forEach(feature => {
         features_sub.forEach(feature_sub => {
             if (feature_sub.features_id == feature.id) {
-                const Log = new log({
+                let Log = new log({
                     _id: mongoose.Types.ObjectId(),
                     node_id: feature.id,
                     action: feature_sub.id,
+                    created_at: Date.now()
                     });
+                    console.log(Log);
                 Log.save((err, Log) => {
                     if (err) {
                       console.log(err)
